8e36cd293d6f1d99817648e7089be36a799a00e1
aimetu <41498149+aimetu@users.noreply.github.com>
2025-02-16 22:08:25 +0800

    Update diy-part1.sh

--- king/diy-part1.sh
+++ king/diy-part1.sh
@@ -18,3 +18,5 @@
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 # echo "src-git neko https://github.com/nosignals/openwrt-neko.git;main" >> "feeds.conf.default"
 echo "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main" >> "feeds.conf.default"
+# echo "src-git nekobox https://github.com/Thaolga/openwrt-nekobox.git;main" >> "feeds.conf.default"
+# echo "src-git mosdns https://github.com/sbwml/luci-app-mosdns.git;v5" >> "feeds.conf.default"
