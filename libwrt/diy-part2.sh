a60e78c96358760ab21bbcb9e18909b6ffce97fc
aimetu <41498149+aimetu@users.noreply.github.com>
2025-02-17 12:07:39 +0800

    Update diy-part2.sh

--- libwrt/diy-part2.sh
+++ libwrt/diy-part2.sh
@@ -85,6 +85,9 @@ git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/luci-app-mosdns
 # git clone https://github.com/nikkinikki-org/OpenWrt-nikki.git package/luci-app-nikki
 
 # 添加 OpenClash 插件（ dev 版 ）
-# rm -rf feeds/luci/applications/luci-app-openclash
+rm -rf feeds/luci/applications/luci-app-openclash
 # git clone -b dev https://github.com/vernesong/OpenClash.git package/luci-app-openclash
-# git clone --depth=1 https://github.com/vernesong/OpenClash.git -b dev package/luci-app-openclash
+git clone --depth=1 https://github.com/vernesong/OpenClash.git -b dev package/luci-app-openclash
+
+./scripts/feeds update -a
+./scripts/feeds install -a
