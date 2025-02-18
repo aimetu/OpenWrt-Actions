af1ce0c40752fcf981b72beb9d96ad2ffd9f62c4
aimetu <41498149+aimetu@users.noreply.github.com>
2025-02-18 10:42:58 +0800

    Update diy-part2.sh

--- lede-ax1800/diy-part2.sh
+++ lede-ax1800/diy-part2.sh
@@ -31,7 +31,7 @@ sed -i "s/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=6.6/g" target/linux/qualcommax/M
 # 取消 bootstrap 为默认主题，添加 argon 主题设置为默认
 rm -rf feeds/luci/themes/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
-# git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-theme-argon-config
+git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-theme-argon-config
 
 # sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 # sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
@@ -44,9 +44,9 @@ git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-a
 # git clone https://github.com/sirpdboy/luci-app-advancedplus.git package/luci-app-advancedplus
 
 # 删除自带 AdguardHome 文件，添加 AdguardHome 广告过滤插件
-# rm -rf feeds/packages/net/adguardhome
-# rm -rf feeds/luci/applications/luci-app-adguardhome
-# git clone https://github.com/xptsp/luci-app-adguardhome package/luci-app-adguardhome
+rm -rf feeds/packages/net/adguardhome
+rm -rf feeds/luci/applications/luci-app-adguardhome
+git clone https://github.com/xptsp/luci-app-adguardhome package/luci-app-adguardhome
 # git clone https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
 # git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
 
@@ -74,6 +74,7 @@ git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-a
 
 # 添加 nikki 插件
 # git clone https://github.com/nikkinikki-org/OpenWrt-nikki.git package/luci-app-nikki
+# git clone --depth=1 https://github.com/nikkinikki-org/OpenWrt-nikki.git -b main package/luci-app-nikki
 
 # 添加 OpenClash 插件（ dev 版 ）
 rm -rf feeds/luci/applications/luci-app-openclash
