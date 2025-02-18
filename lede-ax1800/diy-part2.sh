c547e7a1bcb5e04074d4444aae0baeacf0a561dc
aimetu <41498149+aimetu@users.noreply.github.com>
2025-02-18 13:26:58 +0800

    Update diy-part2.sh

--- lede-ax1800/diy-part2.sh
+++ lede-ax1800/diy-part2.sh
@@ -45,8 +45,8 @@ git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-ap
 
 # 删除自带 AdguardHome 文件，添加 AdguardHome 广告过滤插件
 # rm -rf feeds/packages/net/adguardhome
-rm -rf feeds/luci/applications/luci-app-adguardhome
-git clone https://github.com/xptsp/luci-app-adguardhome package/luci-app-adguardhome
+# rm -rf feeds/luci/applications/luci-app-adguardhome
+# git clone https://github.com/xptsp/luci-app-adguardhome package/luci-app-adguardhome
 # git clone https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
 # git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
 
