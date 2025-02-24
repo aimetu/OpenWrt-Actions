7196d0bc6e268429f44508c34d8afde3d630a6bc
aimetu <41498149+aimetu@users.noreply.github.com>
2025-02-24 17:36:42 +0800

    更新 diy-part2.sh

--- libwrt/diy-part2.sh
+++ libwrt/diy-part2.sh
@@ -97,7 +97,7 @@ git clone --depth=1 https://github.com/vernesong/OpenClash.git -b dev package/lu
 # alist
 rm -rf feeds/packages/net/alist
 rm -rf feeds/luci/applications/luci-app-alist
-git clone https://github.com/sbwml/luci-app-alist package/alist
+git clone https://github.com/sbwml/luci-app-alist package/luci-app-alist
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
