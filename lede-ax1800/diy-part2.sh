9bb9eece831ad3100aa6091b062e361c2791a5d9
aimetu <41498149+aimetu@users.noreply.github.com>
2025-04-22 22:47:07 +0800

    Update diy-part2.sh

--- lede-ax1800/diy-part2.sh
+++ lede-ax1800/diy-part2.sh
@@ -100,5 +100,10 @@ rm -rf feeds/luci/applications/luci-app-openclash
 # git clone -b dev https://github.com/vernesong/OpenClash.git package/luci-app-openclash
 git clone --depth=1 https://github.com/vernesong/OpenClash.git -b dev package/luci-app-openclash
 
+# alist
+rm -rf feeds/packages/net/alist
+rm -rf feeds/luci/applications/luci-app-alist
+git clone https://github.com/sbwml/luci-app-alist package/luci-app-alist
+
 ./scripts/feeds update -a
 ./scripts/feeds install -a
