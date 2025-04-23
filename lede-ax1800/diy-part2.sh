53eae1db128eb726969bc5d4ba1cf21c803a3535
aimetu <41498149+aimetu@users.noreply.github.com>
2025-04-23 14:29:39 +0800

    Update diy-part2.sh

--- lede-ax1800/diy-part2.sh
+++ lede-ax1800/diy-part2.sh
@@ -60,7 +60,7 @@ git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-ap
 # git clone https://github.com/sirpdboy/luci-app-advancedplus.git package/luci-app-advancedplus
 
 # 删除自带 AdguardHome 文件，添加 AdguardHome 广告过滤插件
-# rm -rf feeds/packages/net/adguardhome
+rm -rf feeds/packages/net/adguardhome
 # rm -rf feeds/luci/applications/luci-app-adguardhome
 # git clone https://github.com/xptsp/luci-app-adguardhome package/luci-app-adguardhome
 # git clone https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
@@ -83,7 +83,8 @@ git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/luci-app-mosdns
 rm -rf feeds/packages/net/smartdns
 rm -rf feeds/luci/applications/luci-app-smartdns
 git clone https://github.com/pymumu/openwrt-smartdns.git  feeds/packages/net/smartdns
-git clone -b lede https://github.com/pymumu/luci-app-smartdns.git feeds/luci/applications/luci-app-smartdns
+git clone https://github.com/pymumu/luci-app-smartdns.git feeds/luci/applications/luci-app-smartdns
+# git clone -b lede https://github.com/pymumu/luci-app-smartdns.git feeds/luci/applications/luci-app-smartdns
 
 # nekobox
 # git clone https://github.com/Thaolga/openwrt-nekobox.git package/openwrt-nekobox
