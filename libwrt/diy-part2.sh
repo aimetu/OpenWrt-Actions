a16728d63188d86a947ec7db8bfa91a30f6e6d47
aimetu <41498149+aimetu@users.noreply.github.com>
2025-05-07 00:31:40 +0800

    Update diy-part2.sh

--- libwrt/diy-part2.sh
+++ libwrt/diy-part2.sh
@@ -18,9 +18,10 @@ sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
 
 # 修改 wifi 无线名称
 sed -i "s/ImmortalWrt/OpenWrt/g" package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
+sed -i "s/LiBwrt/OpenWrt/g" package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
 
 # 修改 wifi 无线名称 & 密码
-sed -i "s/OWRT/OpenWrt/g" target/linux/qualcommax/base-files/etc/uci-defaults/990_set-wireless.sh
+sed -i "s/BASE_SSID='.*'/BASE_SSID='OpenWrt'/g" target/linux/qualcommax/base-files/etc/uci-defaults/990_set-wireless.sh
 sed -i "s/12345678/password/g" target/linux/qualcommax/base-files/etc/uci-defaults/990_set-wireless.sh
 
 # 最大连接数修改为65535
