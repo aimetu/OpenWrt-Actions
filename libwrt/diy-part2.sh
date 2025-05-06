910e253ec3fc0e2973dca631d5d4a510ad3fcdad
aimetu <41498149+aimetu@users.noreply.github.com>
2025-05-06 09:10:35 +0800

    Update diy-part2.sh

--- libwrt/diy-part2.sh
+++ libwrt/diy-part2.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+# !/bin/bash
 #
 # Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 #
@@ -11,16 +11,16 @@
 #
 
 # 修改 device 设备名称
-sed -i "s/LibWrt/OpenWrt/g" package/base-files/files/bin/config_generate
+sed -i "s/hostname='.*'/hostname='OpenWrt'/g" package/base-files/files/bin/config_generate
 
 # 默认网关 ip 地址修改
 sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
 
 # 修改 wifi 无线名称
-sed -i "s/LiBwrt/OpenWrt/g" package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
+sed -i "s/ImmortalWrt/OpenWrt/g" package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
 
 # 修改 wifi 无线名称 & 密码
-sed -i "s/LiBwrt/OpenWrt/g" target/linux/qualcommax/base-files/etc/uci-defaults/990_set-wireless.sh
+sed -i "s/OWRT/OpenWrt/g" target/linux/qualcommax/base-files/etc/uci-defaults/990_set-wireless.sh
 sed -i "s/12345678/password/g" target/linux/qualcommax/base-files/etc/uci-defaults/990_set-wireless.sh
 
 # 最大连接数修改为65535
@@ -33,7 +33,7 @@ sed -i "s/12345678/password/g" target/linux/qualcommax/base-files/etc/uci-defaul
 # 修改 wifi 默认打开
 # sed -i "s/disabled='${defaults ? 0 : 1}'/disabled='0'/g" package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
 
-# 更换 6.6 内核为 6.1 内核
+# 更换 KERNEL 内核版本
 # sed -i "s/KERNEL_PATCHVER:=6.6/KERNEL_PATCHVER:=6.1/g" target/linux/qualcommax/Makefile
 
 # samba 解除 root 限制
@@ -47,20 +47,6 @@ sed -i "s/12345678/password/g" target/linux/qualcommax/base-files/etc/uci-defaul
 # sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 # sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
 
-# 添加 kucat 主题，搭配 luci-app-advancedplus 设置参数 
-# git clone -b js https://github.com/sirpdboy/luci-theme-kucat.git package/luci-theme-kucat
-
-# 添加 advanced 系统设置插件
-# git clone https://github.com/sirpdboy/luci-app-advanced.git package/luci-app-advanced
-# git clone https://github.com/sirpdboy/luci-app-advancedplus.git package/luci-app-advancedplus
-
-# 删除自带 AdguardHome 文件，添加 AdguardHome 广告过滤插件
-# rm -rf feeds/packages/net/adguardhome
-# rm -rf feeds/luci/applications/luci-app-adguardhome
-# git clone https://github.com/TanZhiwen2001/luci-app-adguardhome package/luci-app-adguardhome
-# git clone https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
-# git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
-
 # 更新 golang 依赖（ mosdns & alist 插件 )
 # rm -rf feeds/packages/lang/golang
 # git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
@@ -68,6 +54,13 @@ sed -i "s/12345678/password/g" target/linux/qualcommax/base-files/etc/uci-defaul
 # 替换 geodata 依赖
 # rm -rf feeds/packages/net/v2ray-geodata
 # git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+
+# AdguardHome
+# rm -rf feeds/packages/net/adguardhome
+# rm -rf feeds/luci/applications/luci-app-adguardhome
+# git clone https://github.com/TanZhiwen2001/luci-app-adguardhome package/luci-app-adguardhome
+# git clone https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
+# git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
 
 # mosdns
 rm -rf feeds/packages/net/mosdns
@@ -80,24 +73,37 @@ git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/luci-app-mosdns
 # git clone https://github.com/pymumu/openwrt-smartdns.git package/smartdns
 # git clone https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
 
+# OpenClash（ dev 版 ）
+rm -rf feeds/luci/applications/luci-app-openclash
+git clone --depth=1 https://github.com/vernesong/OpenClash.git -b dev package/luci-app-openclash
+# git clone -b dev https://github.com/vernesong/OpenClash.git package/luci-app-openclash
+
+# passwall(2)
+rm -rf feeds/luci/applications/luci-app-passwall
+# rm -rf feeds/luci/applications/luci-app-passwall2
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
+# git clone https://github.com/xiaorouji/openwrt-passwall2.git package/luci-app-passwall2
+git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/luci-app-passwall
+
+# nikki
+git clone --depth=1 https://github.com/nikkinikki-org/OpenWrt-nikki.git -b main package/luci-app-nikki
+
 # nekobox
 # git clone --depth=1 https://github.com/Thaolga/openwrt-nekobox.git -b main package/openwrt-nekobox
 
 # neko
 # git clone --depth=1 https://github.com/nosignals/openwrt-neko.git -b main package/openwrt-neko
 
-# nikki
-# git clone --depth=1 https://github.com/nikkinikki-org/OpenWrt-nikki.gitt -b main package/luci-app-nikki
-
-# OpenClash（ dev 版 ）
-rm -rf feeds/luci/applications/luci-app-openclash
-# git clone -b dev https://github.com/vernesong/OpenClash.git package/luci-app-openclash
-git clone --depth=1 https://github.com/vernesong/OpenClash.git -b dev package/luci-app-openclash
-
 # alist
 # rm -rf feeds/packages/net/alist
 # rm -rf feeds/luci/applications/luci-app-alist
 # git clone https://github.com/sbwml/luci-app-alist package/luci-app-alist
+
+# jdCloud ax6600 led screen ctrl
+# git clone --depth=1 https://github.com/NONGFAH/luci-app-athena-led package/luci-app-athena-led
+
+# filemanager（ 文件管理 ）
+# git clone https://github.com/sbwml/luci-app-filemanager package/luci-app-filemanager
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
