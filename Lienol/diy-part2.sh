a3cb9468c2db55dd4ed504bfe7b3d361fe239621
aimetu <41498149+aimetu@users.noreply.github.com>
2025-05-18 11:34:41 +0800

    Update diy-part2.sh

--- Lienol/diy-part2.sh
+++ Lienol/diy-part2.sh
@@ -53,71 +53,70 @@ sed -i 's/services/control/g' feeds/luci/applications/luci-app-wifischedule/root
 
 # 取消 bootstrap 为默认主题，添加 argon 主题设置为默认
 rm -rf feeds/luci/themes/luci-theme-argon
-git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
-git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
-# git clone https://github.com/sbwml/luci-theme-argon -b openwrt-24.10 package/luci-theme-argon
+git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
+# git clone --depth=1 -b openwrt-24.10 https://github.com/sbwml/luci-theme-argon package/luci-theme-argon
 
 # sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 # sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
 
 # 更新 golang 依赖（ mosdns & alist )
 # rm -rf feeds/packages/lang/golang
-# git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
+# git clone --depth=1 -b 23.x https://github.com/sbwml/packages_lang_golang feeds/packages/lang/golang
 
 # 替换 geodata 依赖
 # rm -rf feeds/packages/net/v2ray-geodata
-# git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+# git clone --depth=1 https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
 # adguardhome
 rm -rf feeds/other/luci-app-adguardhome
 rm -rf feeds/packages/net/adguardhome
 rm -rf feeds/luci/applications/luci-app-adguardhome
-git clone --depth 1 https://github.com/sirpdboy/luci-app-adguardhome -b lua package/luci-app-adguardhome
-# git clone https://github.com/xptsp/luci-app-adguardhome package/luci-app-adguardhome
-# git clone https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
-# git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
+git clone --depth=1 -b lua https://github.com/sirpdboy/luci-app-adguardhome package/luci-app-adguardhome
+# git clone --depth=1 https://github.com/xptsp/luci-app-adguardhome package/luci-app-adguardhome
+# git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
+# git clone --depth=1 https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
 
 # mosdns
 rm -rf feeds/packages/net/mosdns
 rm -rf feeds/luci/applications/luci-app-mosdns
-git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/luci-app-mosdns
+git clone --depth=1 -b v5 https://github.com/sbwml/luci-app-mosdns package/luci-app-mosdns
 
 # smartdns
 rm -rf feeds/packages/net/smartdns
 rm -rf feeds/luci/applications/luci-app-smartdns
-git clone https://github.com/pymumu/openwrt-smartdns.git  feeds/packages/net/smartdns
-git clone https://github.com/pymumu/luci-app-smartdns.git feeds/luci/applications/luci-app-smartdns
+git clone --depth=1 https://github.com/pymumu/openwrt-smartdns.git  feeds/packages/net/smartdns
+git clone --depth=1 https://github.com/pymumu/luci-app-smartdns.git feeds/luci/applications/luci-app-smartdns
 
 # passwall(2)
 rm -rf feeds/packages/net/shadowsocks-libev
 rm -rf feeds/luci/applications/luci-app-passwall
 rm -rf feeds/luci/applications/luci-app-passwall2
-git clone https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
-git clone https://github.com/xiaorouji/openwrt-passwall2.git package/luci-app-passwall2
-git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2.git package/luci-app-passwall2
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall
 
 # openclash（ dev 版 ）
 rm -rf feeds/luci/applications/luci-app-openclash
-git clone --depth=1 https://github.com/vernesong/OpenClash.git -b dev package/luci-app-openclash
+git clone --depth=1 -b dev https://github.com/vernesong/OpenClash.git package/luci-app-openclash
 # git clone -b dev https://github.com/vernesong/OpenClash.git package/luci-app-openclash
 
 # homeproxy
-git clone --depth 1 https://github.com/immortalwrt/homeproxy.git -b dev  package/luci-app-homeproxy
+git clone --depth=1 -b dev https://github.com/immortalwrt/homeproxy.git package/luci-app-homeproxy
 
 # nikki
-# git clone https://github.com/nikkinikki-org/OpenWrt-nikki.git package/luci-app-nikki
-# git clone --depth=1 https://github.com/nikkinikki-org/OpenWrt-nikki.git -b main package/luci-app-nikki
+git clone --depth=1 -b main https://github.com/nikkinikki-org/OpenWrt-nikki.git package/luci-app-nikki
 
 # nekobox
 # git clone --depth=1 https://github.com/Thaolga/openwrt-nekobox.git package/openwrt-nekobox
 
 # neko
-# git clone --depth=1 https://github.com/nosignals/openwrt-neko.git -b main package/openwrt-neko
+# git clone --depth=1 https://github.com/nosignals/openwrt-neko.git package/openwrt-neko
 
 # alist
 # rm -rf feeds/packages/net/alist
 # rm -rf feeds/luci/applications/luci-app-alist
-# git clone --depth 1 https://github.com/sbwml/luci-app-alist.git package/luci-app-alist
+# git clone --depth=1 https://github.com/sbwml/luci-app-alist.git package/luci-app-alist
 
 # filemanager（ 文件管理 ）
 # git clone --depth=1 https://github.com/sbwml/luci-app-filemanager package/luci-app-filemanager
