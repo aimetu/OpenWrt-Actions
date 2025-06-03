169f4c194294b92eb6f98415ababdd42cb6bebbe
aimetu <41498149+aimetu@users.noreply.github.com>
2025-06-03 13:21:06 +0800

    Update diy-part2.sh

--- libwrt/diy-part2.sh
+++ libwrt/diy-part2.sh
@@ -133,5 +133,7 @@ git clone --depth=1 -b main https://github.com/xiaorouji/openwrt-passwall-packag
 # filemanager
 # git clone --depth=1 -b main https://github.com/sbwml/luci-app-filemanager.git package/luci-app-filemanager
 
+sed -i 's/--set=llvm\.download-ci-llvm=true/--set=llvm.download-ci-llvm=false/' feeds/packages/lang/rust/Makefile
+
 ./scripts/feeds update -a
 ./scripts/feeds install -a
