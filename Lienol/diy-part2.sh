4bdefc2da03f884d63b0aac8e765d5a49b1f19be
aimetu <41498149+aimetu@users.noreply.github.com>
2025-06-29 12:59:46 +0800

    Update diy-part2.sh

--- Lienol/diy-part2.sh
+++ Lienol/diy-part2.sh
@@ -135,9 +135,9 @@ git clone --depth=1 -b dev https://github.com/immortalwrt/homeproxy.git package/
 # git clone --depth=1 -b main https://github.com/sbwml/luci-app-filemanager.git package/luci-app-filemanager
 
 # luci-app-diskman
-rm -rf feeds/other/luci-app-diskman
-rm -rf feeds/luci/applications/luci-app-diskman
-git clone --depth=1 https://github.com/lisaac/luci-app-diskman.git package/luci-app-diskman
+# rm -rf feeds/other/luci-app-diskman
+# rm -rf feeds/luci/applications/luci-app-diskman
+# git clone --depth=1 https://github.com/lisaac/luci-app-diskman.git package/luci-app-diskman
 
 # 修复 jdCloud ax6600 无限重启
 rm -rf package/kernel/mac80211/patches/nss/ath11k/999-900-bss-transition-handling.patch
