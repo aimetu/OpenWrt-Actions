aa6a115cce65b306bd6030a44ca8087cc97a9fad
aimetu <jinqianto@outlook.com>
2025-12-02 10:21:19 +0800

    Update diy-part1.sh

--- lienol/diy-part1.sh
+++ lienol/diy-part1.sh
@@ -15,8 +15,9 @@
 
 # Add a feed source
 # echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
-# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' >>feeds.conf.default
-# echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git' >>feeds.conf.default
+# echo 'src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;main' >>feeds.conf.default
+# echo 'src-git passwall2_luci https://github.com/xiaorouji/openwrt-passwall2.git;main' >>feeds.conf.default
+# echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >>feeds.conf.default
 # echo "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main" >> "feeds.conf.default"
 # echo "src-git neko https://github.com/nosignals/openwrt-neko.git;dev" >> "feeds.conf.default"
 # echo "src-git nekobox https://github.com/Thaolga/openwrt-nekobox.git;main" >> "feeds.conf.default"
