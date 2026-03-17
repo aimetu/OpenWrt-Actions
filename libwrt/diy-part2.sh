fe9c1aafa1326af40c8f37f3c42fbb3344bd5988
aimetu <jinqianto@outlook.com>
2026-03-17 11:14:15 +0800

    Update diy-part2.sh

--- libwrt/diy-part2.sh
+++ libwrt/diy-part2.sh
@@ -123,7 +123,7 @@ git clone --depth=1 -b main https://github.com/zerolabnet/SSClash.git package/lu
 # git clone --depth=1 -b dev https://github.com/immortalwrt/homeproxy.git package/luci-app-homeproxy
 
 # singbox-ui ( SingBox Kernel )
-# git clone --depth=1 -b main https://github.com/ang3el7z/luci-app-singbox-ui.git package/luci-app-singbox-ui
+git clone --depth=1 -b main https://github.com/ang3el7z/luci-app-singbox-ui.git package/luci-app-singbox-ui
 
 # momo ( SingBox Kernel ) OpenWrt ≥ 24.10
 # git clone --depth=1 -b main https://github.com/nikkinikki-org/OpenWrt-momo.git package/luci-app-momo
