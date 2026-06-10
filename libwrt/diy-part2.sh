816808931740139b2641c64192d707941acd1910
aimetu <jinqianto@outlook.com>
2026-06-10 15:24:59 +0800

    Update diy-part2.sh

--- libwrt/diy-part2.sh
+++ libwrt/diy-part2.sh
@@ -165,9 +165,8 @@ git clone --depth=1 https://github.com/tty228/luci-app-wechatpush.git package/lu
 # easytier
 git clone --depth=1 https://github.com/EasyTier/luci-app-easytier.git package/luci-app-easytier
 
-# luci-app-turboacc （带sfe & 不带sfe）
-# curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
-# curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh --no-sfe
+# luci-app-turboacc
+git clone --depth=1 https://github.com/chenmozhijin/turboacc.git package/luci-app-turboacc
 
 # webdav ( openwrt ≥ 24.10 ) 
 # git clone --depth=1 -b openwrt-24.10 https://github.com/sbwml/luci-app-webdav.git package/luci-app-webdav
