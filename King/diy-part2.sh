08033a0b778421296f60711d563e220886e341f4
aimetu <jinqianto@outlook.com>
2026-09-19 22:22:18 +0800

    Update diy-part2.sh

--- King/diy-part2.sh
+++ King/diy-part2.sh
@@ -156,9 +156,9 @@ rm -rf package/luci-app-ssr-plus/{mosdns,mihomo,xray-core,hysteria,chinadns-ng,v
 # git clone --depth=1 https://github.com/sbwml/openwrt_helloworld.git package/openwrt_helloworld
 
 # daed ( eBPF 透明代理引擎, 需修改内核大小为 12M ）
-# rm -rf feeds/packages/net/{dae,daed}
-# rm -rf feeds/luci/applications/{luci-app-dae,luci-app-daed}
-# git clone --depth=1 https://github.com/kenzok8/openwrt-daede.git package/luci-app-daede
+rm -rf feeds/packages/net/{dae,daed}
+rm -rf feeds/luci/applications/{luci-app-dae,luci-app-daed}
+git clone --depth=1 https://github.com/kenzok8/openwrt-daede.git package/luci-app-daede
 # git clone --depth=1 https://github.com/QiuSimons/luci-app-daed.git package/luci-app-daed
 
 # honk ( eBPF 透明代理引擎, 需修改内核大小为 12M ）
